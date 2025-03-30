c048ec85461dc33a7bcfbd033ad74245e6639664
tauantcamargo <lukyhenson@gmail.com>
2025-03-30 21:59:17 +0200

    Integrate MMKV DevTools into RootLayout for enhanced state management debugging

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,8 @@ import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
 import CartButton from "@/domains/cart/components/CartButton";
+import { useMMKVDevTools } from "@dev-plugins/react-native-mmkv";
+import { storage } from "@/shared/store/mmkv";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -13,6 +15,9 @@ const queryClient = new QueryClient({
 
 export default function RootLayout() {
 	useReactQueryDevTools(queryClient);
+	useMMKVDevTools({
+		storage,
+	});
 
 	return (
 		<QueryClientProvider client={queryClient}>
